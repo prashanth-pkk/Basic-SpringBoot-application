486895fc1160b6b7077f86b0115916a04025e6ce
prashanth.kanukuntla <prashanth.kanukuntla@italentdigital.com>
2024-11-26 20:05:22 +0530

    basic spring boot application

---
+++ src/main/java/com/pk/BasicSbApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSbApplication.class, args);
+	}
+
+}
